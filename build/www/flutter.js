6e5ee0c45a7cbdfa7d42165c24feb8025d61212b
Erik Antonio Alarcon Pinedo <ealarconp@regionancash.gob.pe>
2022-08-28 18:06:45 -0500

    10 commit

--- build/www/flutter.js
+++ build/www/flutter.js
@@ -42,7 +42,7 @@ _flutter.loader = null;
      */
     loadEntrypoint(options) {
       const {
-        entrypointUrl = "main.dart.js?v=1",
+        entrypointUrl = "main.dart.js",
         serviceWorker,
       } = (options || {});
       return this._loadWithServiceWorker(entrypointUrl, serviceWorker);
